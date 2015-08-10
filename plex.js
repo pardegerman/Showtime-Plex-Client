198e56b0a78f426051048d5b75bf1bdb4909ab12
Pär Degerman <par@degerman.org>
2015-08-10 19:15:49 +0100

    Updated for movian v4.10

--- plex.js
+++ plex.js
@@ -1,7 +1,8 @@
 /**
  *  Plex for showtime by Björn Dahlgren (bjorn@dahlgren.at)
+ *  updated for movian v 4.10 by Pär Degerman (par@degerman.org)
  *
- *  Copyright 2011 Björn Dahlgren
+ *  Copyright 2011 Björn Dahlgren, 2015 Pär Degerman
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,10 +17,12 @@
  *  limitations under the License.
  *
  */
+
+var XML = require('showtime/xml');
  
 (function(plugin) {
 
-	this.authUrl = "https://my.plexapp.com/users/sign_in.xml";
+	this.authUrl = "https://plex.tv/users/sign_in.xml";
 	this.currentServerUrl;
 	this.currentServerToken;
 	this.myPlexToken;
@@ -46,13 +49,13 @@
      * @param item XML element to search for an image
      */
     function getIcon(item) {
-        if (item.@thumb != null && item.@thumb.toString() != "")
-            return get_image_url(item.@thumb);
-        if (item.@art != null && item.@art.toString() != "")
-            return get_image_url(item.@art);
-        if (item.@banner != null && item.@banner.toString() != "")
-            return get_image_url(item.@banner);
-        return plugin.path + "plex-logo.png";
+	if (item['@thumb'])
+            return get_image_url(item['@thumb']);
+        if (item['@art'])
+            return get_image_url(item['@art']);
+        if (item['@banner'])
+            return get_image_url(item['@banner']);
+	return plugin.path + "plex-logo.png";
     }
 
     /*
@@ -62,97 +65,102 @@
         page.contents = "items";
         page.type = "directory";
 
-        var doc = new XML(request_directory(key));
+	var doc = XML.parse(request_directory(key));
 
         page.metadata.logo = getIcon(doc);
-        page.metadata.title = doc.@title2.toString();
+        page.metadata.title = doc.MediaContainer['@title2'];
 
-        for each (var season in doc.Directory) {
+	var seasons = doc.MediaContainer.filterNodes('Directory');
+        for (var i = 0; i < seasons.length; i++) {
+	    var season = seasons[i];
             var metadata = {
-                title: season.@title.toString(),
-                description: season.@summary.toString(),
+                title: season['@title'],
+                description: season['@summary'],
                 icon: getIcon(season)
             };
-            page.appendItem("plex:season:" + season.@key, "directory", metadata);
-        }
+            page.appendItem("plex:season:" + season['@key'], "directory", metadata);
+        };
         page.loading = false;
     });
 
     /*
-     * View for a TV seasion, lists episodes
+     * View for a TV season, lists episodes
      */
     plugin.addURI("plex:season:([A-Za-z0-9\/]*)", function(page, key) {            
         page.contents = "video";
         page.type = "directory";
 
-        var doc = new XML(request_directory(key));
+        var doc = XML.parse(request_directory(key));
 
         page.metadata.logo = getIcon(doc);
-        page.metadata.title = doc.@title2.toString();
+        page.metadata.title = doc.MediaContainer['@title2'];
         
         var showName;
-        if (doc.@parentTitle != "")
-            showName = doc.@parentTitle.toString();
+        if (doc.MediaContainer['@parentTitle'])
+            showName = doc.MediaContainer['@parentTitle'];
         else
-            showName = doc.@title2.toString();
+            showName = doc.MediaContainer['@title2'];
 
-        for each (var video in doc.Video) {
+	var videos = doc.MediaContainer.filterNodes('Video');
+        for (var i = 0; i < videos.length; i++) {
+	    var video = videos[i];
             var numbering;
-            if (video.@parentIndex != null && video.@parentIndex.toString() != "")
-                numbering = video.@parentIndex + "x" + video.@index;
+            if (video['@parentIndex'])
+                numbering = video['@parentIndex'] + "x" + video['@index'];
             else
-                numbering = doc.@parentIndex + "x" + video.@index;
+                numbering = doc.MediaContainer['@parentIndex'] + "x" + video['@index'];
             
-            var title = numbering + " - " + video.@title;
+            var title = numbering + " - " + video['@title'];
             var metadata = {
                 title: title,
-                description: video.@summary,
-                duration: showtime.durationToString(parseInt(video.@duration.toString()) / 1000),
+                description: video['@summary'],
+                duration: showtime.durationToString(parseInt(video['@duration']) / 1000),
                 icon: getIcon(video)
             };
-            var url = get_video_url(video.Media.Part[0].@key);
+            var url = get_video_url(video.Media.Part['@key']);
             page.appendItem(getVideo(url, showName + " - " + title), "video", metadata);
         }
         page.loading = false;
     });
 
     plugin.addURI("plex:section:([0-9]*)", function(page, section) {            
-
         page.type = "directory";
 
-        var doc = new XML(request_directory("/library/sections/" + section + "/all/"));
+        var doc = XML.parse(request_directory("/library/sections/" + section + "/all/"));
 
         page.metadata.logo = plugin.path + "plex-logo.png";
-        page.metadata.title = doc.@title1;
+        page.metadata.title = doc.MediaContainer['@title1'];
 
-        if (doc.@viewGroup == "movie") {
+        if (doc.MediaContainer['@viewGroup'] == "movie") {
             page.contents = "video";
 
-            for each (var video in doc.Video) {
+	    var videos = doc.MediaContainer.filterNodes('Video');
+            for (var i = 0; i < videos.length; i++) {
+		var video = videos[i];
                 var metadata = {
-                    title: video.@title.toString(),
-                    description: video.@summary.toString(),
-                    duration: showtime.durationToString(parseInt(video.@duration.toString()) / 1000),
+                    title: video['@title'],
+                    description: video['@summary'],
+                    duration: showtime.durationToString(parseInt(video['@duration']) / 1000),
                     icon: getIcon(video)
                 };
-                var url = get_video_url(video.Media.Part[0].@key);
-                page.appendItem(getVideo(url, video.@title.toString()), "video", metadata);
+                var url = get_video_url(video.Media.Part['@key']);
+                page.appendItem(getVideo(url, video['@title']), "video", metadata);
             }
-        } else if (doc.@viewGroup == "show") {
+        } else if (doc.MediaContainer['@viewGroup'] == "show") {
             page.contents = "items";
-            for each (var show in doc.Directory) {
+	    var shows = doc.MediaContainer.filterNodes('Directory');
+            for (var i = 0; i < shows.length; i++) {
+		var show = shows[i];
                 var metadata = {
-                    title: show.@title.toString(),
-                    description: show.@summary.toString(),
+                    title: show['@title'],
+                    description: show['@summary'],
                     icon: getIcon(show)
                 };
-                page.appendItem("plex:show:" + show.@key, "directory", metadata);
+                page.appendItem("plex:show:" + show['@key'], "directory", metadata);
             }
         }
         page.loading = false;
     });
-    
-    
 
     plugin.addURI("plex:start:(.*):(.*):([0-9]*)", function(page, token, host, port) {            
         page.type = "directory";
@@ -161,61 +169,80 @@
         this.currentServerUrl = "http://" + host + ":" + port;
         this.currentServerToken = token;
 
-        var doc = new XML(request_directory("/library/sections/"));
+	var doc = XML.parse(request_directory("/library/sections/"));
 
         page.metadata.logo = plugin.path + "plex-logo.png";
-        page.metadata.title = doc.@title1.toString();
+        page.metadata.title = doc.MediaContainer["@title1"];
 
-        for each (var section in doc.Directory) {
+	var directories = doc.MediaContainer.filterNodes('Directory');
+	for (var i = 0; i < directories.length; i++) {
+	    var section = directories[i];
             var metadata = {
-                title: section.@title.toString(),
+                title: section["@title"],
                 icon: getIcon(section)
             };
-            page.appendItem("plex:section:" + section.@key, "directory", metadata);
+            page.appendItem("plex:section:" + section["@key"], "directory", metadata);
         }
         page.loading = false;
     });
     
     plugin.addURI("plex:auth", function(page) {
-	    var deviceID = showtime.currentVersionString; // Need to figure out proper device id
-	    var headers = {"X-Plex-Client-Identifier": deviceID, "X-Plex-Product": "Showtime Plex Plugin"};
-	    var response = showtime.httpPost(authUrl, {}, {}, headers);
-	    
-	    var doc = new XML(response.toString());
-	    this.myPlexToken = doc["authentication-token"].toString();
-	    
-	    var serversURL = "https://my.plexapp.com/pms/servers.xml";
-	    response = showtime.httpGet(serversURL, {"auth_token": myPlexToken});
-	    var doc = new XML(response.toString());
-	    	    	    
-	    page.type = "directory";
+	var deviceID = showtime.deviceId;
+	var response = showtime.httpReq(authUrl, {
+	    debug: true,
+	    method: 'POST',
+	    headers: {
+	        'X-Plex-Client-Identifier': deviceID,
+		'X-Plex-Product': 'movian Plex Plugin'
+	    }
+	});
+	var doc = XML.parse(response);
+	this.myPlexToken = doc.user['@authenticationToken'];
+	
+	var serversURL = "https://plex.tv/pms/servers.xml";
+	response = showtime.httpReq(serversURL, {
+	    debug: true,
+	    args: {
+	        'auth_token': this.myPlexToken
+	    }
+	});
+
+	var doc = XML.parse(response);
+    	    	    
+	page.type = "directory";
         page.contents = "items";
-	    page.metadata.title = "myPlex Servers";
-	    
-	    for each (var server in doc.Server) {
+	page.metadata.title = "myPlex Servers";
+
+	var servers = doc.MediaContainer.filterNodes('Server');
+	for(var i = 0; i < servers.length; i++) {
+	    var server = servers[i];
             var metadata = {
-                title: server.@name.toString()
+                title: server["@name"]
             };
-            var token = server.@accessToken.toString();
-            if (token == "") token = myPlexToken;
+	    var token = server["@accessToken"];
+            if (!token) token = this.myPlexToken;
             
-            var path = "plex:start:" + token + ":" + server.@host + ":" + server.@port;
-            page.appendItem(path, "directory", metadata);
+            var path = "plex:start:" + token + ":" + server["@host"] + ":" + server["@port"];
+	    page.appendItem(path, "directory", metadata);
         }
         page.loading = false;
     });
     
     plugin.addHTTPAuth(authUrl, function(authreq) {
-    	var auth = getAuthCredentials("My Plex", "We need to authenticate with myPlex", false);
-	    var authData = "Basic " + base64_encode(auth.username + ":" + auth.password);
-	    authreq.rawAuth(authData);
+    	var auth = plugin.getAuthCredentials("My Plex", "We need to authenticate with myPlex", false);
+	var authData = "Basic " + base64_encode(auth.username + ":" + auth.password);
+	authreq.rawAuth(authData);
     });
 
     plugin.URIRouting = true;
     plugin.createService("Plex", "plex:auth", "video", true, plugin.path + "plex-logo.png");
     
     function request_directory(url) {
-	    return showtime.httpGet(currentServerUrl + url, {"X-Plex-Token": currentServerToken}).toString()
+        return showtime.httpReq(currentServerUrl + url, {
+	    args: {
+	        "X-Plex-Token": currentServerToken
+	    }
+	});
     }
     
     function get_image_url(url) {
